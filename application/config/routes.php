d914148423368c3be5dbe72aa63bba9ed927226b
Mirna Nabih <Mirna@Mirnas-MacBook-Pro-2.local>
2014-12-09 15:08:12 +0200

    none

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,13 +3,37 @@
 
 // $appended_lang = "(?:[a-zA-Z]{2}/?)?"; 
 // $prepended_lang = "(?:[a-zA-Z]{2}/)?"; 
-
-$route['default_controller'] = "admin";
-
 // $route["$appended_lang"] = "home";
+$route['default_controller'] = "home";
 
 
 $route['404_override'] = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $route['admin'] = 'admin';
 $route['admin/script'] = 'admin/script';
@@ -53,117 +77,3 @@ $route['admin/users/new'] = 'admin/createUser';
 $route['admin/projects/new'] = 'admin/createProject';
 $route['admin/checkpasswordchange.php'] = 'admin/checkpasswordchange';
 $route['admin/users/(:any)'] = "admin/userProfile";
-
-
-
-
-// $route['mail'] = 'home/sendMail';
-
-
-
-
-//$route['admin/authenticate'] = 'admin/authenticate';
-
-// $route['logout'] = 'home/logout';
-// $route["$appended_lang/logout"] = "home/logout";
-
-
-
-// $route['validate/(:any)'] = "home/validateToken";
-// $route['subscribeuser'] = 'home/subscribeuser';
-// $route['insertPropertyAlert'] = "home/insertPropertyAlert";
-
-// $route['register'] = 'home/register';
-// $route["$appended_lang/register"] = "home/register";
-
-
-// $route['profile'] = 'home/profile';
-// $route["$appended_lang/profile"] = "home/profile";
-
-
-// $route['shareProperty'] = 'home/shareProperty';
-// $route["$appended_lang/shareProperty"] = "home/shareProperty";
-
-
-// $route['viewAllProperties'] = 'home/viewAllProperties';
-// $route["$appended_lang/viewAllProperties"] = "home/viewAllProperties";
-
-// $route['viewAllProperties/(:any)'] = 'home/viewAllProperties';
-// $route["$appended_lang/viewAllProperties/(:any)"] = "home/viewAllProperties";
-
-// $route['propertyDetails'] = 'home/propertyDetails';
-// $route["$appended_lang/propertyDetails"] = "home/propertyDetails";
-
-// $route['propertyDetails/(:any)'] = 'home/propertyDetails';
-// $route["$appended_lang/propertyDetails/(:any)"] = "home/propertyDetails";
-
-// $route['careers'] = 'home/careers';
-// $route["$appended_lang/careers"] = "home/careers";
-
-// $route['uploadCV'] = 'home/uploadCV';
-// $route["$appended_lang/uploadCV"] = "home/uploadCV";
-
-// $route['uploadCV/(:any)'] = 'home/uploadCV';
-// $route["$appended_lang/uploadCV/(:any)"] = "home/uploadCV";
-
-// $route['joinUs'] = 'home/joinUs';
-// $route["$appended_lang/joinUs"] = "home/joinUs";
-
-
-// $route['marketIndex'] = 'home/marketIndex';
-// $route["$appended_lang/marketIndex"] = "home/marketIndex";
-
-// $route['auction'] = 'home/auction';
-// $route["$appended_lang/auction"] = "home/auction";
-
-
-// $route['compareProperties'] = 'home/compareProperties';
-// $route["$appended_lang/compareProperties"] = "home/compareProperties";
-
-// $route['trainingCenter'] = 'home/trainingCenter';
-// $route["$appended_lang/trainingCenter"] = "home/trainingCenter";
-
-// $route['getDistricts'] = 'home/getDistricts';
-// $route['getPropertyTypes'] = 'home/getPropertyTypes';
-// $route['displayOffice'] = 'home/displayOffice';
-// $route['displayMap'] = 'home/displayMap';
-
-// $route['forgotPassword'] = 'home/forgotPassword';
-// $route["$appended_lang/forgotPassword"] = "home/forgotPassword";
-
-// $route['resetpassword/(:any)'] = 'home/resetpassword';
-// $route["$appended_lang/resetpassword/(:any)"] = "home/resetpassword";
-
-// $route['offices'] = 'home/offices';
-// $route["$appended_lang/offices"] = "home/offices";
-
-// $route['about'] = 'home/about';
-// $route["$appended_lang/about"] = "home/about";
-
-// $route['franchise'] = 'home/franchise';
-// $route["$appended_lang/franchise"] = "home/franchise";
-
-// $route['insertContact'] = 'home/insertContact';
-// $route["$appended_lang/insertContact"] = "home/insertContact";
-
-// $route['insertFavorite'] = 'home/insertFavorite';
-// $route["$appended_lang/insertFavorite"] = "home/insertFavorite";
-
-// $route['deleteFavorite'] = 'home/deleteFavorite';
-// $route["$appended_lang/deleteFavorite"] = "home/deleteFavorite";
-
-// $route["$appended_lang/home"] = "home";
-// $route["createExcel"] = "home/createExcel";
-
-
-// //Temporary
-// $route["newsletterSingle"] = "home/newsletterSingle";
-// $route["newsletterBanner"] = "home/newsletterBanner";
-// $route["newsletterProperties"] = "home/newsletterProperties";
-
-
-// $route['getFeaturedProperties'] = 'home/getFeaturedProperties';
-
-
-/* End of file routes.php */
-/* Location: ./application/config/routes.php */
---
+++ application/views/home.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Outsource</title>
+		<link rel="stylesheet" href="css/normalize.css"/>
+		<link href="<?= base_url(); ?>application/static/css/style.css" rel="stylesheet"/>
+	</head>
+	<body>
+
+		<div class="row col-lg-12 col-md-12 col-sm-12 col-xs-12 top_row">
+			<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+				<img src="images/logo.jpg" alt="" class="img-responsive">
+			</div>
+			<div class="col-lg-6 col-lg-offset-1 col-md-6 col-md-offset-1 col-sm-6 col-sm-offset-1 col-xs-6 col-xs-offset-1" style="background-color:grey; height:100px; width:60%;">
+			</div>
+		</div>
+
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding:0;">
+			<nav class="navbar navbar-default top">
+	    	    <div class="container-fluid">
+	    	        <div class="navbar-header">
+	    	            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	    	              <span class="sr-only">Toggle navigation</span>
+	    	              <span class="icon-bar"></span>
+	    	              <span class="icon-bar"></span>
+	    	              <span class="icon-bar"></span>
+	    	            </button>
+	    	        </div>     
+			        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="padding:0;">
+			            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			            	<div class="row menu">
+			                	<ul class="nav navbar-nav col-lg-9 col-md-9 col-sm-9 col-xs-12">
+						            <li class="col-lg-1 col-md-1 col-sm-2 col-xs-12 col-lg-offset-4 col-md-offset-4 col-sm-offset-1 text-center">HOME</li>
+						            <li class="col-lg-1 col-md-1 col-sm-2 col-xs-12 text-center">PROJECTS</li>
+						            <li class="col-lg-1 col-md-1 col-sm-2 col-xs-12 text-center">SAHRAWY</li>
+						            <li class="col-lg-1 col-md-1 col-sm-2 col-xs-12 text-center">RAYOS</li>
+						            <li class="col-lg-1 col-md-1 col-sm-2 hidden-xs text-center" style="width: 10%;">CONTACT US</li>
+						            <li class="col-xs-12 visible-xs text-center">CONTACT US</li>
+						        </ul>
+						        <div class="col-sm-3 col-md-3 pull-right">
+						        <form class="navbar-form" role="search" style="margin: 0;">
+							        <div class="input-group">
+							            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+							            <div class="input-group-btn">
+							                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+							            </div>
+							        </div>
+						        </form>
+			            	</div>
+			            </div>
+			        </div> 
+	    	    </div>
+	    	</nav>
+		</div>
+
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-bottom:5%; margin-left:0; margin-right:0; padding:0;">
+			<ul class="bxslider">
+				<li><img src="images/image1.jpg" style="max-height:500px;"/></li>
+				<li><img src="images/image2.jpg" style="max-height:500px;"/></li>
+				<li><img src="images/image3.jpg" style="max-height:500px;"/></li>
+			</ul> 
+		</div>
+
+		<div class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div>
+				<img src="images/logo.jpg" alt="" class="img-responsive col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-4 col-xs-offset-4">
+			</div>
+		</div>
+
+		<div class="row col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<img src="images/tours1.png" alt="" class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="width: 13%; margin-left:10%;">
+			<img src="images/booking.png" alt="" class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="width: 13%;">
+			<img src="images/tours1.png" alt="" class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="width: 13%;">
+			<img src="images/booking.png" alt="" class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="width: 13%;">
+			<img src="images/tours1.png" alt="" class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="width: 13%;">
+			<img src="images/booking.png" alt="" class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="width: 13%;">
+		</div>
+
+
+		<footer>
+			<div class="footer_text hidden-sm hidden-xs col-lg-12 col-md-12" style="padding-left:0; padding-right:0;">
+				<div class="col-lg-1 col-md-1 text-center" style="cursor: pointer; padding:0;">
+					HOME
+				</div>
+				<div class="col-lg-1 col-md-1 text-center" style="cursor: pointer; padding:0;">
+					PROJECTS
+				</div>
+				<div class="col-lg-1 col-md-1 text-center" style="cursor: pointer; padding:0;">
+					SAHRAWY
+				</div>
+				<div class="col-lg-1 col-md-1 text-center" style="cursor: pointer; padding:0;">
+					RAYOS
+				</div>
+				<div class="col-lg-1 col-md-1 text-center" style="cursor: pointer; padding:0;">
+					CONTACT US
+				</div>	
+				<div class="col-lg-3 col-lg-offset-3 col-md-3 col-md-offset-3">
+					<form action="" method="post">
+						<div class="col-lg-8 col-md-8" style="padding:0;">
+							<input type="email" name="email" placeholder="YOUR EMAIL" class="subscribe_input rounded">
+						</div>
+						<div class="col-lg-2 col-md-2" style="padding:0;">
+							<input type="submit" name="subscribe" value="SUBSCRIBE" class="subscribe_button">
+						</div>
+					</form>
+				</div>		
+				<div class="col-lg-1 col-md-1">
+					<img class="img-responsive col-lg-4 col-md-4" style="padding:5%; padding-top:2%;" src="images/twitter.jpg">
+					<img class="img-responsive col-lg-4 col-md-4" style="padding:5%; padding-top:2%;" src="images/youtube.jpg">
+					<img class="img-responsive col-lg-4 col-md-4" style="padding:5%; padding-top:2%;" src="images/facebook.jpg">
+				</div>	
+			</div>
+			<div class="row footer_text col-sm-12 col-xs-12 hidden-md hidden-lg">
+				<div class="col-sm-6 col-xs-6" style="padding-top: 0.5%;">
+					<form action="" method="post">
+						<div class="col-sm-8 col-xs-8" style="padding:0;">
+							<input type="email" name="email" placeholder="YOUR EMAIL" class="subscribe_input rounded">
+						</div>
+						<div class="col-sm-2 col-xs-2" style="padding:0;">
+							<input type="submit" name="subscribe" value="SUBSCRIBE" class="subscribe_button">
+						</div>
+					</form>
+				</div>		
+				<div class="col-sm-3 col-sm-offset-3 col-xs-3 col-xs-offset-3">
+					<img class="img-responsive col-sm-3 col-xs-3" style="padding:5%; padding-top:2%;" src="images/twitter.jpg">
+					<img class="img-responsive col-sm-3 col-xs-3" style="padding:5%; padding-top:2%;" src="images/youtube.jpg">
+					<img class="img-responsive col-sm-3 col-xs-3" style="padding:5%; padding-top:2%;" src="images/facebook.jpg">
+				</div>	
+			</div>
+		</footer>
+
+		<script src="js/main.js"></script>
+		
+
+		<script>
+			$(document).ready(function(){
+			  //$('.bxslider').bxSlider();
+			});
+		</script>
+	</body>
+</html>
